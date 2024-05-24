c996058f9a188342bd782e4d749f9bc6cfdd8a7d
FirdausMusafaHaqi <firdausmusafa21@gmail.com>
2024-05-24 18:01:52 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt soluta aut provident repellendus explicabo fugiat blanditiis, omnis aliquid? Eligendi tempore commodi iure quidem odio voluptatum enim, dolorum sint earum natus!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
